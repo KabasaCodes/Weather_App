df305b0aaab15f9f83fc41d7a858f0622e2281da
Adegeye Idowu <aWX1088416@china.huawei.com>
2023-09-05 15:43:40 +0100

    adding javascript to the app

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const apiKey = "8fd61e014f7b3159f96643ac6b32c335";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=lagos";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+let searchBox = document.querySelector(".search input")
+let searchBtn = document.querySelector(".search button")
+console.log(searchBtn);
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
     let data = await response.json();
 
 
     console.log(data);
 
     let cityTown = document.querySelector(".city").innerHTML = data.name;
-    let townTem = document.querySelector(".temp").innerHTML = data.main.temp;
-    let townHum = document.querySelector(".humidity").innerHTML = data.main.humidity;
-    let townWind = document.querySelector(".wind").innerHTML = data.wind.speed;
+    let townTem = document.querySelector(".temp").innerHTML =Math.round(data.main.temp)  + "°c";
+    let townHum = document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    let townWind = document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
 }
-checkWeather()
+
+searchBtn.addEventListener("click", ()=>{
+checkWeather(searchBox.value)
+})
+
 
 
